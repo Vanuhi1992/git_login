7ff42d88820f1d6b5fca77c440f7a8bc547f500e
Sargis <saqogh1993@gmail.com>
2020-01-24 00:52:58 +0400

    Added time sleep for sign in button

--- git.py
+++ git.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
 from unittest import TestCase
 import time
 
@@ -13,6 +14,7 @@ class GitLogin(TestCase):
     def test_valid_login(self):
         sign_in_button = self.driver.find_element_by_xpath("//a[@class='HeaderMenu-link no-underline mr-3']")
         sign_in_button.click()
+        time.sleep(3)
 
         username = self.driver.find_element_by_xpath("//input[@id='login_field']")
         password = self.driver.find_element_by_xpath("//input[@id='password']")
